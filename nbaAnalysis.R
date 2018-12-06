f02520f4565590d69319be7a9b85a4ec97081190
William Fritz <wfritz@ND.EDU>
2018-12-06 05:55:26 -0500

    update

--- nbaAnalysis.R
+++ nbaAnalysis.R
@@ -15,13 +15,20 @@
 # write.csv(df, file="./data/teamStats1998-2018.csv")
 
 # comment above and uncomment below if we already generated joint csv
-df <- read.csv("./data/teamSats1998-2017.csv")
+df <- read.csv("./data/teamStats1998-2018.csv", stringsAsFactors = F)
+df$X <-NULL #do not want this column (index)
 
 ## First we will attempt a Population average model
 ## (performing a GEE analysis of data)
 library(gee)
-df.clusterOrder <- df[order(df$team_name),]
-mod.gee <- gee(plyf~win_loss_pct+movA+nrtgA, family="binomial", id=team_name, corstr="unstructured", tol=0.01, data=df.clusterOrder)
+
+# we must take the Spurs out of the dataframe since they have made the playoffs every single year since 1998
+# thus, the model cannot be fit properly since there is no knowledge about the Spurs not making the playoffs
+df.NoSpurs <- subset(df, team_name!='San Antonio Spurs')
+
+df.clusterOrder <- df.NoSpurs[order(df.NoSpurs$team_name),]
+df.clusterOrder$team_name <- as.factor(df.clusterOrder$team_name)
+mod.gee <- gee(plyf~win_loss_pct, family="binomial", id=team_name, corstr="exchangeable", data=df.clusterOrder)
 
 
-## Next we will atttempt random cluster
+## Next we will atttempt cluster specific (random effects)
